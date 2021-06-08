d37182108a1117452637bbd0530be08978bcab40
FrenzyyCode <85103351+FrenzyyCode@users.noreply.github.com>
2021-06-08 11:42:54 +0300

    Slots oyunu eklendi

---
+++ v12/slots.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const { stripIndents } = require('common-tags');
+const slots = ['🍇', '🍊', '🍐', '🍒', '🍋'];
+
+exports.run = function(client, message) {
+
+	var slot1 = slots[Math.floor(Math.random() * slots.length)];
+	var slot2 = slots[Math.floor(Math.random() * slots.length)];
+	var slot3 = slots[Math.floor(Math.random() * slots.length)];
+	
+	if (slot1 === slot2 && slot1 === slot3) {
+		message.channel.send(stripIndents`
+		${slot1} : ${slot2} : ${slot3}
+		Tebrikler, kazandınız!
+		`); 
+	} else {
+		message.channel.send(stripIndents`
+		${slot1} : ${slot2} : ${slot3}
+		Eyvah, kaybettin!
+		`);	
+	}
+
+};
+
+exports.conf = {
+  enabled: false, 
+  guildOnly: false, 
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'slots'
+};
